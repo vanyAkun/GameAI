126b0bea67c2c2b18868bfec9d44fa091a7fe9ab
Vanesa Mendez <73304237+vanyAkun@users.noreply.github.com>
2023-11-20 22:05:28 +0000

    Canvas

--- Assets/Scripts/AgentController.cs
+++ Assets/Scripts/AgentController.cs
@@ -2,27 +2,85 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class AgentController : MonoBehaviour
 {
-  NavMeshAgent agent;
+    public float movementSpeed = 10f;
+
+    NavMeshAgent agent;
+
+    public float fireRate = 0.75f;
+    public GameObject bulletPrefab;
+    public Transform bulletPosition;
+    float nextFire;
+
+    [HideInInspector]
+    public int health = 100;
+
+    public Slider healthBar;
+
+
+    // Start is called before the first frame update
     void Start()
     {
-        agent = GetComponent<NavMeshAgent>();   
-    }
+        agent = GetComponent<NavMeshAgent>();
 
-    // Update is called once per frame
+    }
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);    
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
-            if(Physics.Raycast(ray, out hit))
+            if (Physics.Raycast(ray, out hit))
             {
                 agent.SetDestination(hit.point);
             }
         }
     }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Space))
+            Fire();
+    }
+
+   
+
+
+    void Fire() 
+    {
+        if (Time.time > nextFire) 
+        {
+            nextFire = Time.time + fireRate;
+
+            GameObject bullet = Instantiate(bulletPrefab, bulletPosition.position, Quaternion.identity);
+
+            bullet.GetComponent<BulletController>()?.InitializeBullet(transform.rotation * Vector3.forward);
+
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Bullet"))
+        {
+            BulletController bullet = collision.gameObject.GetComponent<BulletController>();
+           // TakeDamage(bullet.damage);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        health -= damage;
+        healthBar.value = health / 100f;
+
+    }
+  
+
+
+
 }
