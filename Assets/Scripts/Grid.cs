d6442e51792a7bda1386b021f2e1ab729733c070
Vanesa Mendez <73304237+vanyAkun@users.noreply.github.com>
2023-11-21 15:50:25 +0000

    gdfg

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -9,9 +9,43 @@ public class Grid : MonoBehaviour
     public float nodeRadius;
     Node[,] grid;
 
+    float nodeDiameter;
+    int gridSizeX, gridSizeY;
+
+    private void Start()// calculate how many nodes qwe can fit in ou script
+    {
+        nodeDiameter = nodeRadius * 2;
+        gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
+        gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
+        CreateGrid();
+    }
+    void CreateGrid()
+    {
+        grid = new Node[gridSizeX, gridSizeY]; 
+        Vector3 worldBottomLeft = transform.position -Vector3.right * gridWorldSize.x / 2 - Vector3.forward * gridWorldSize.y / 2;
+
+        for(int x = 0; x< gridSizeX; x++)//loop through to check collision to see if theyre walkable or not
+        {
+            for (int y = 0; y < gridSizeY; y++)
+            {
+                Vector3 worldPoint = worldBottomLeft+ Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);//now we getting each point that a node is going to occupy
+                bool walkable = !(Physics.CheckSphere(worldPoint, nodeRadius, unwalkableMask));
+                grid[x,y]= new Node(walkable, worldPoint);  
+            }
+
+        }
+    }
     void OnDrawGizmos()
     {
         Gizmos.DrawWireCube(transform.position,new Vector3(gridWorldSize.x,1,gridWorldSize.y));
+        if (grid != null)
+        {
+            foreach (Node n in grid)
+            {
+                Gizmos.color = (n.walkable) ? Color.white : Color.red;
+                Gizmos.DrawCube (n.worldPosition, Vector3.one * (nodeDiameter-.1f));
+            }
+        }
     }
 
 }
