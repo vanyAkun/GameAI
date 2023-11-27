6ae6f931f15c831bc935d576dfa7e75d5ef5fe08
Vanesa Mendez <73304237+vanyAkun@users.noreply.github.com>
2023-11-27 13:45:27 +0000

    Revert "items spawn correctly"
    This reverts commit 90c3422e03eb9f28212a43d4e5de1a1581fffb11.

--- Assets/Scripts/Terrain/MapGenerator.cs
+++ Assets/Scripts/Terrain/MapGenerator.cs
@@ -25,13 +25,12 @@ public class MapGenerator : MonoBehaviour
     public Vector2 offset;
     public float meshHeightMultiplier;
     public AnimationCurve meshHeightCurve;
-   
+    public AnimationCurve heightCurve;
 
     public NavMeshSurface navMeshSurface;
     public MeshCollider meshCollider;
     public MeshFilter meshFilter;
 
-    public GameObject playerPrefab;
     public GameObject heartPrefab;
     public GameObject starPrefab;
     public GameObject treePrefab;
@@ -65,67 +64,8 @@ public class MapGenerator : MonoBehaviour
         EnemyStarSpawn();
         ChestSpawn();
     }
-    private void StarsSpawn()
-    {
-        SpawnItems(nOfStars, starPrefab, "Star");
-    }
-    private void HeartsSpawn()
-    {
-        SpawnItems(nOfHearts, heartPrefab, "Heart");
-    }
 
-    private void TreesSpawn()
-    {
-        SpawnItems(nOfTrees, treePrefab, "Tree");
-    }
 
-    private void GemsSpawn()
-    {
-        SpawnItems(nOfGems, gemPrefab, "Gem");
-    }
-
-    private void EnemyStarSpawn()
-    {
-        SpawnItems(nOfEnemyStars, enemyStarPrefab, "EnemyStar");
-    }
-
-    private void ChestSpawn()
-    {
-        SpawnItems(nOfChests, chestPrefab, "Chest");
-    }
-
-    private void SpawnItems(int numberOfItems, GameObject prefab, string tag)
-    {
-        for (int i = 0; i < numberOfItems; i++)
-        {
-            bool itemTooClose;
-            Vector3 itemPosition;
-
-            do
-            {
-                itemTooClose = false;
-                float randomX = Random.Range(-26f, 26f);
-                float randomZ = Random.Range(-26f, 26f);
-
-                // Calculate the correct height at the position
-                float height = TerrainHeightAtPoint(randomX, randomZ);
-
-                // Use this height for the y-coordinate
-                itemPosition = new Vector3(randomX, height, randomZ);
-
-                foreach (GameObject item in GameObject.FindGameObjectsWithTag(tag))
-                {
-                    if (Vector3.Distance(item.transform.position, itemPosition) < minDistance)
-                    {
-                        itemTooClose = true;
-                        break;
-                    }
-                }
-            } while (itemTooClose);
-
-            Instantiate(prefab, itemPosition, Quaternion.identity);
-        }
-    }
     public void GenerateMap()
     {
         float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight,seed, noiseScale,octaves,persistance,lacunarity,offset);
@@ -184,30 +124,184 @@ public class MapGenerator : MonoBehaviour
             octaves = 0;
         }
     }
-
- 
-    private float TerrainHeightAtPoint(float x, float z)
+    private void TreesSpawn()
     {
-        RaycastHit hit;
-        float y = 0;
-        int terrainLayer = 1 << LayerMask.NameToLayer("TerrainLayer"); // Replace with your terrain's layer
-        Vector3 rayStart = new Vector3(x, 1000, z);
-
-        // Debug line for visualization (visible in the Scene view)
-        Debug.DrawLine(rayStart, rayStart + Vector3.down * 1500, Color.red, 5f);
-
-        // Cast a ray straight down from the start point with layer mask
-        if (Physics.Raycast(rayStart, Vector3.down, out hit, 1500, terrainLayer))
+        for (int i = 0; i < nOfTrees; i++)
         {
-            y = hit.point.y;
-        }
-        else
-        {
-            // Handle the case where the ray doesn't hit the terrain
-        }
+            bool itemTooClose;
+            Vector3 treePosition;
 
-        return y;
+            do
+            {
+                itemTooClose = false;
+
+                // Generate random position within the specified boundaries
+                float randomX = Random.Range(-26f, 26f); // X-axis: -26 to 26
+                float randomY = Random.Range(-26f, 26f); // Z-axis: -26 to 26
+                float height = SampleTerrain(randomX, randomY);
+
+                treePosition = new Vector3(randomX, height, randomY);
+
+                // Check for minimum distance from other trees
+                foreach (GameObject tree in GameObject.FindGameObjectsWithTag("Tree"))
+                {
+                    if (Vector3.Distance(tree.transform.position, treePosition) < minDistance)
+                    {
+                        itemTooClose = true;
+                        break;
+                    }
+                }
+            } while (itemTooClose);
+
+            Instantiate(treePrefab, treePosition, Quaternion.identity);
+        }
     }
+    private void HeartsSpawn()
+    {
+        for (int i = 0; i < nOfHearts; i++)
+        {
+            float randomX = Random.Range(0f, mapWidth);
+            float randomY = Random.Range(0f, mapHeight);
+            Vector3 randomPosition = new Vector3(randomX, 0f, randomY);
+
+            bool ItemTooClose = false;
+            foreach (GameObject heart in GameObject.FindGameObjectsWithTag("Heart"))
+            {
+                if (Vector3.Distance(heart.transform.position, randomPosition) < minDistance)
+                {
+                    ItemTooClose = true;
+                    break;
+                }
+            }
+
+            if (!ItemTooClose)
+            {
+                Vector3 heartPosition = new Vector3(randomX, SampleTerrain(randomX, randomY), randomY);
+                Instantiate(heartPrefab, heartPosition, Quaternion.identity);
+            }
+        }
+    }
+    private void StarsSpawn()
+    {
+        for (int i = 0; i < nOfStars; i++)
+        {
+            float randomX = Random.Range(0f, mapWidth);
+            float randomY = Random.Range(0f, mapHeight);
+            Vector3 randomPosition = new Vector3(randomX, 0f, randomY);
+
+            bool ItemTooClose = false;
+            foreach (GameObject star in GameObject.FindGameObjectsWithTag("Star"))
+            {
+                if (Vector3.Distance(star.transform.position, randomPosition) < minDistance)
+                {
+                    ItemTooClose = true;
+                    break;
+                }
+            }
+
+            if (!ItemTooClose)
+            {
+                Vector3 starPosition = new Vector3(randomX, SampleTerrain(randomX, randomY), randomY);
+                Instantiate(starPrefab, starPosition, Quaternion.identity);
+            }
+        }
+    }
+    private void GemsSpawn()
+    {
+        for (int i = 0; i < nOfGems; i++)
+        {
+            float randomX = Random.Range(0f, mapWidth);
+            float randomY = Random.Range(0f, mapHeight);
+            Vector3 randomPosition = new Vector3(randomX, 0f, randomY);
+
+            bool ItemTooClose = false;
+            foreach (GameObject gem in GameObject.FindGameObjectsWithTag("Gem"))
+            {
+                if (Vector3.Distance(gem.transform.position, randomPosition) < minDistance)
+                {
+                    ItemTooClose = true;
+                    break;
+                }
+            }
+
+            if (!ItemTooClose)
+            {
+                Vector3 gemPosition = new Vector3(randomX, SampleTerrain(randomX, randomY), randomY);
+                Instantiate(gemPrefab, gemPosition, Quaternion.identity);
+            }
+        }
+    }
+    private void EnemyStarSpawn()
+    {
+        for (int i = 0; i < nOfEnemyStars; i++)
+        {
+            float randomX = Random.Range(0f, mapWidth);
+            float randomY = Random.Range(0f, mapHeight);
+            Vector3 randomPosition = new Vector3(randomX, 0f, randomY);
+
+            bool ItemTooClose = false;
+            foreach (GameObject enemyStar in GameObject.FindGameObjectsWithTag("EnemyStar"))
+            {
+                if (Vector3.Distance(enemyStar.transform.position, randomPosition) < minDistance)
+                {
+                    ItemTooClose = true;
+                    break;
+                }
+            }
+
+            if (!ItemTooClose)
+            {
+                Vector3 enemyStarPosition = new Vector3(randomX, SampleTerrain(randomX, randomY), randomY);
+                Instantiate(enemyStarPrefab, enemyStarPosition, Quaternion.identity);
+            }
+        }
+    }
+    private void ChestSpawn()
+    {
+        for (int i = 0; i < nOfChests; i++)
+        {
+            float randomX = Random.Range(0f, mapWidth);
+            float randomY = Random.Range(0f, mapHeight);
+            Vector3 randomPosition = new Vector3(randomX, 0f, randomY);
+
+            bool ItemTooClose = false;
+            foreach (GameObject chest in GameObject.FindGameObjectsWithTag("Chest"))
+            {
+                if (Vector3.Distance(chest.transform.position, randomPosition) < minDistance)
+                {
+                    ItemTooClose = true;
+                    break;
+                }
+            }
+
+            if (!ItemTooClose)
+            {
+                Vector3 chestPosition = new Vector3(randomX, SampleTerrain(randomX, randomY), randomY);
+                Instantiate(chestPrefab, chestPosition, Quaternion.identity);
+            }
+        }
+    }
+    private float SampleTerrain(float x, float y)
+    {
+        // Ensure x and y are within the bounds of the map
+        int mapX = Mathf.Clamp((int)x, 0, mapWidth - 1);
+        int mapY = Mathf.Clamp((int)y, 0, mapHeight - 1);
+
+        // Generate the noise map if it doesn't exist
+        if (noiseMap == null)
+        {
+            noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
+        }
+
+        // Retrieve the height value from the noise map
+        float heightValue = noiseMap[mapX, mapY];
+
+        // Apply the height multiplier and the height curve
+        return heightCurve.Evaluate(heightValue) * meshHeightMultiplier;
+    }
+
+    // Add a private field to store the noise map
+    private float[,] noiseMap;
 }
 [System.Serializable]
 public struct TerrainType
